4db70d53b76509db7629de37f05a2bf151f840ee
LookerKy <nagaraz07@gmail.com>
2021-05-18 14:44:22 +0900

    section 13

--- src/section13-2.py
+++ src/section13-2.py
@@ -1,5 +1,17 @@
 import random
 import time
+import sqlite3
+import datetime
+
+conn = sqlite3.connect('/Users/ino/Project/python3/python_basic/src/resource/records.db', isolation_level=None)
+
+cursor = conn.cursor()
+
+cursor.execute("create table if not exists records("
+               "id integer primary key autoincrement,"
+               "cor_cnt integer,"
+               "record text,"
+               "regdate text)")
 
 word = []
 
@@ -30,7 +42,7 @@ while n <= 5:
     print()
     if str(x).strip() == str(q).strip():
         print("Pass!")
-        cor_cnt +=1
+        cor_cnt += 1
     else:
         print("Wrong")
 
@@ -44,6 +56,9 @@ if cor_cnt >= 3:
     print("합격")
 else:
     print("불합격")
+
+cursor.execute("insert into records('cor_cnt', 'record', 'regdate') values (?, ?, ?)",
+               (cor_cnt, et, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 print("총 게임시간 :", et, "초", "정답 개수 : {}".format(cor_cnt))
 
